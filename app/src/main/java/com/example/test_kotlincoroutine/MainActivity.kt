7f7e28db56d92ffdc7af762a119463befd31914c
AmrNasser <amr931111@gmail.com>
2023-10-19 00:12:17 +0200

    async & awaiy()

--- app/src/main/java/com/example/test_kotlincoroutine/MainActivity.kt
+++ app/src/main/java/com/example/test_kotlincoroutine/MainActivity.kt
@@ -6,29 +6,56 @@ import android.util.Log
 import android.widget.TextView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myTextV : TextView
+  //  lateinit var myTextV : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myTextV = findViewById(R.id.tx1)
-        runBlocking { printMyTextAfterDelay("Amr nasser") }
+       // myTextV = findViewById(R.id.tx1)
+       // runBlocking { printMyTextAfterDelay("Amr nasser") }
+
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val dataUser = async { getUserFromDatabase() }
+                val localUser = async { getUserFromNetwork() }
+
+
+                if (dataUser.await()==localUser.await()){
+                    Log.d("here", "equal: ")
+
+                }else {
+                    Log.d("here", "not equal: ")
+                }
+            }
+            Log.d("here", time.toString())
+        }
 
     }
 
-    private suspend fun printMyTextAfterDelay(myText : String){
+   /*  private suspend fun printMyTextAfterDelay(myText : String){
         GlobalScope.launch(Dispatchers.IO) {
         delay(2000)
             withContext(Dispatchers.Main){
                 myTextV.text = myText
             }
         }
+    } */
+
+    private suspend fun getUserFromNetwork():String{
+        delay(2000)
+        return "Amr"
+    }
+    private suspend fun getUserFromDatabase():String{
+        delay(2000)
+        return "Amr"
     }
 }
